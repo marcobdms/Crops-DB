41edbea0cdbd92eeb66399c23439ebf6414bdaf3
marcobdms <el_marco4@hotmail.com>
2023-09-12 16:49:57 +0200

    get para mostrar los cultivos por su id

--- routes.js
+++ routes.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const routes = express.Router()
 
+
+routes.get('/:id', (req, res) =>{
+    const id = req.params.id;
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+
+        conn.query('SELECT * FROM Plants WHERE id = ?', [id], (err, rows) => {
+            if (err) return res.send(err);
+            if (rows.length === 0) {
+                res.status(404).json({ error: 'Elemento no encontrado' });
+            } else {
+                res.json(rows[0]);
+            }
+        });
+    });
+});
+
+
 routes.get('/', (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
@@ -28,7 +46,7 @@ routes.delete('/:id', (req, res)=>{
         conn.query('DELETE FROM Plants WHERE id = ?', [req.params.id], (err, rows)=>{
             if(err) return res.send(err)
 
-            res.send('datos borrado')
+            res.send('datos borrados')
         })
     })
 })
